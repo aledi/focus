0e90662463af1443708c777d3e2f0b9959caff9d
Eduardo Cristerna <eduardo@schoolhub.com>
2016-07-11 19:55:13 -0500

    first approach

--- src/elements/navigation-bar.php
+++ src/elements/navigation-bar.php
@@ -10,6 +10,34 @@
         </li>
       <li><a href="alta-panel.php">Nuevo Panel</a></li>
       <li><a href="liga-panel-panelista.php">Ligar Panelistas</li>
-      <li style="float:right"><a class="active signOutButton" href="#about">Sign Out</a></li>
+      <li style="float:right"><button id="logOutButton" class="active">Sign Out</button></li>
     </ul>
+    <!-- <ul id = 'logOutButton' class='nav navbar-nav navbar-right'>
+        <li><a href='logIn.php'>Cerrar Sesión <span class='glyphicon glyphicon-log-out'></span></a></li>
+    </ul> -->
 </div>
+
+<script type = 'text/javascript'>
+
+$('#logOutButton').on('click', function (event) {
+    event.preventDefault();
+
+    var parameters = {
+        'action': 'LOG_OUT'
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '../api/controller.php',
+        data: parameters,
+        dataType: 'json',
+        success: function (obj) {
+            if (obj.status === "SUCCESS") {
+                alert("¡Hasta pronto!");
+                location.replace("signin.php");
+            }
+        }
+    });
+});
+
+</script>
