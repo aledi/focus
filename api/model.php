79c5b7d38ea00cb977a60d79bb079dfdf45057b9
Eduardo Cristerna <eduardo@schoolhub.com>
2016-07-14 20:42:02 -0500

    ...

--- api/model.php
+++ api/model.php
@@ -174,7 +174,7 @@ function fetchPanel () {
         $response = array();
 
         while ($row = $result->fetch_assoc()) {
-            $client = array('id' => (int)$row['id'], 'nombre' => $row['nombre'], 'fechaInicio' => $row['fechaInicio'], (int)'cliente' => $row['cliente'], (int)'creador' => $row['creador']);
+            $client = array('id' => (int)$row['id'], 'nombre' => $row['nombre'], 'fechaInicio' => $row['fechaInicio'], 'cliente' => (int)$row['cliente'], 'creador' => (int)$row['creador']);
             $response[] = $client;
         }
 
