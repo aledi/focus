e38a2a9b09e8b172f7adb0f0426d2ed4deac42d5
Tenorio94 <geteca94@gmail.com>
2016-07-18 01:27:41 -0500

    Fixes #16 - Verify Content
    Se agregan las condiciones usando la función que está definida en el
    controlador de "Verify Session", una vez hecho esto, en caso de que no
    esté conectado correctamente, entonces se cambiará la pantalla al signin
    general

--- src/js/liga-panel.js
+++ src/js/liga-panel.js
@@ -1,5 +1,25 @@
 $(document).on('ready', function () {
 
+    parametersSession = }
+        'action' : 'VERIFY_SESSION'
+    };
+
+
+    $.ajax({
+        type: 'POST',
+        url: '../api/controller.php',
+        data: parameters,
+        dataType: 'json',
+        success: function (obj) {
+            console.log(obj.username);
+
+        },
+        error: function (error) {
+            alert("Please login to continue");
+            window.location.replace('signin.php');
+        }
+    });
+
 	parameters = {
 		'action': 'GET_PANEL'
 	};
