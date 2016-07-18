f5c0d24481e27c4ee7217cf0d3c4533d31e29273
Eduardo Cristerna <eduardo@schoolhub.com>
2016-07-18 18:48:23 -0500

    removes

--- src/js/alta-panelistas.js
+++ src/js/alta-panelistas.js
@@ -157,27 +157,5 @@ $(document).on('ready', function () {
             }
         });
     });
-
-    $('#signOutButton').on('click', function (event) {
-        event.preventDefault();
-
-        var parameters = {
-            'action': 'LOG_OUT '
-        };
-
-        $.ajax({
-            type: 'POST',
-            url: '../api/controller.php',
-            data: parameters,
-            dataType: 'json',
-            success: function (obj) {
-                if (obj.status === "SUCCESS") {
-                    alert("¡Hasta pronto!");
-                    location.replace("signin.php");
-                }
-            },
-            error: function (error) {
-            }
-        });
-    });
+    
 });
