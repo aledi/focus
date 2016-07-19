d8cae80ca72a558fd7692fa3688fb50111d7574d
Tenorio94 <geteca94@gmail.com>
2016-07-18 19:25:22 -0500

    Session

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,30 @@
 $(document).on('ready', function () {
+	parameters = {
+        'action' : 'VERIFY_SESSION'
+    };
+
+
+    $.ajax({
+        type: 'POST',
+        url: '../api/controller.php',
+        data: parameters,
+        dataType: 'json',
+        success: function (obj) {
+
+            if (obj.status == 'SUCCESS') {
+                console.log(obj.username);
+            }
+            else {
+                //alert("Please login to continue");
+                window.location.replace('signin.php');
+            }
+
+        },
+        error: function (error) {
+            alert("Please login to continue");
+            window.location.replace('signin.php');
+        }
+    });
 
     $('.signOutButton').on('click', function (event) {
     event.preventDefault();
