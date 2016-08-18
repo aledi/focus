0e81fb951d3faed4626858f0a3e9379f3df0e773
Alex <alexcris.m@gmail.com>
2016-08-17 19:28:17 -0500

    usuarios wrapper

--- src/usuarios.php
+++ src/usuarios.php
@@ -4,6 +4,7 @@
 <head>
   	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   	<link href='css/template.css' type='text/css' rel='stylesheet'/>
+    <link href='css/usuarios.css' type='text/css' rel='stylesheet'/>
   	<script src="js/jquery-1.12.3.js"></script>
     <script src='js/usuarios.js' type='text/javascript'></script>
     <title> Focus - Usuarios</title>
@@ -12,64 +13,66 @@
 <body>
     <?php include_once('elements/header.php');?>
     <section>
-        <ul class="tabs">
-            <li class="tab-link current" data-tab="tab-agregarUsuario">Agregar Usuario</li>
-            <li class="tab-link" data-tab="tab-modificarUsuario">Modificar Usuario</li>
-        </ul>
+        <div class='usuarios-wrapper'>
+            <ul class="tabs">
+                <li class="tab-link current" data-tab="tab-agregarUsuario">Agregar Usuario</li>
+                <li class="tab-link" data-tab="tab-modificarUsuario">Modificar Usuario</li>
+            </ul>
 
-        <div id="tab-agregarUsuario" class="tab-content current">
-            <div class="formHeader">
-                <h2 id='headerTitle'>Agregar Cliente</h2>
-            </div>
-            <div id="emailInput">
-                <p>1. Cuenta de correo electrónico:</p>
-                <input id="email" type="text"/>
+            <div id="tab-agregarUsuario" class="tab-content current">
+                <div class="formHeader">
+                    <h2 id='headerTitle'>Agregar Cliente</h2>
+                </div>
+                <div id="emailInput">
+                    <p>1. Cuenta de correo electrónico:</p>
+                    <input id="email" type="text"/>
+                </div>
+
+                <div id="nombre_admin">
+                    <p>2. Nombre(s) de Pila:</p>
+                    <input id="fName" type="text"/>
+                </div>
+
+                <div id="apellidos_admin">
+                    <p>3. Apellido Paterno:</p>
+                    <input id="lName" type="text"/>
+                </div>
+
+                <div id="apellidos_admin_materno">
+                    <p>4. Apellido Materno:</p>
+                    <input id="lName_materno" type="text"/>
+                </div>
+
+                <div id="usernameInput">
+                    <p>5. Nombre de Usuario:</p>
+                    <input id="username" type="text"/>
+                </div>
+
+                <div class='paswordInput'>
+                    <p> Password: </p>
+                    <input type='password' name='Password' placeholder='••••••••' id='password'>
+                </div>
+
+                <div class='paswordConfirmInput'>
+                    <p> Password Confirmation: </p>
+                    <input type='password' name='Password' placeholder='••••••••' id='passwordConf'>
+                </div>
+
+                <div id="enviarInfo">
+                    <br/>
+                    <button type='submit' id='sendInfoCliente'>Confirmar</button>
+                </div>
+                <div id='feedback'></div>
             </div>
 
-            <div id="nombre_admin">
-                <p>2. Nombre(s) de Pila:</p>
-                <input id="fName" type="text"/>
+            <div id="tab-modificarUsuario" class="tab-content">
+                <div class="formHeader">
+                    <h2>Usuarios Disponibles</h2>
+                </div>
+                <table id="allUsers"></table>
             </div>
-
-            <div id="apellidos_admin">
-                <p>3. Apellido Paterno:</p>
-                <input id="lName" type="text"/>
-            </div>
-
-            <div id="apellidos_admin_materno">
-                <p>4. Apellido Materno:</p>
-                <input id="lName_materno" type="text"/>
-            </div>
-
-            <div id="usernameInput">
-                <p>5. Nombre de Usuario:</p>
-                <input id="username" type="text"/>
-            </div>
-
-            <div class='paswordInput'>
-                <p> Password: </p>
-                <input type='password' name='Password' placeholder='••••••••' id='password'>
-            </div>
-
-            <div class='paswordConfirmInput'>
-                <p> Password Confirmation: </p>
-                <input type='password' name='Password' placeholder='••••••••' id='passwordConf'>
-            </div>
-
-            <div id="enviarInfo">
-                <br/>
-                <button type='submit' id='sendInfoCliente'>Confirmar</button>
-            </div>
-            <div id='feedback'></div>
         </div>
-
-        <div id="tab-modificarUsuario" class="tab-content">
-            <div class="formHeader">
-                <h2>Usuarios Disponibles</h2>
-            </div>
-            <table id="allUsers"></table>
-        </div>
+        <?php include_once('elements/footer.php');?>
     </section>
-    <?php include_once('elements/footer.php');?>
 </body>
 </html>
