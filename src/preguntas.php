043f5b328255562636312fe13ea48b6ffaf266d8
Alex <alexcris.m@gmail.com>
2016-08-19 19:34:54 -0500

    css

--- src/preguntas.php
+++ src/preguntas.php
@@ -13,50 +13,48 @@
 <body>
     <?php include_once('elements/header.php');?>
     <section>
-        <ul class="tabs">
-            <li class="tab-link current" data-tab="tab-agregarEncuesta">Ligar Pregunta</li>
-            <!--<li class="tab-link" data-tab="tab-modificarEncuesta">Modificar Encuesta</li>-->
-        </ul>
+        <div class='preguntas-wrapper'>
+            <ul class='tabs'>
+                <li class='tab-link current' data-tab='tab-agregarEncuesta'>Ligar Pregunta</li>
+            </ul>
 
-        <div id="tab-agregarEncuesta" class="tab-content current">
-            <h2 id='headerTitle'>Ligar Pregunta</h2>
+            <div id='tab-agregarEncuesta' class='tab-content current'>
+                <h2 id='headerTitle'>Ligar Pregunta</h2>
 
-            <div id="questions">
-                <div id="1" class="questionForm">
-                    <div class="questionInput">
-                        <p>Pregunta 1:</p>
-                        <input class="pregunta" name="respuesta1" type="text"/>
-                    </div>
-                    <div class="questionType">
-                        <p>Tipo de pregunta: </p>
-                        <select class="tipoPregunta" name="respuesta1" required>
-                            <option value="1">Abiertas</option>
-                            <option value="2">Selección Única</option>
-                            <option value="3">Ordenamiento</option>
-                            <option value="4">Selección Múltiple</option>
-                        </select>
-                        <p class="feedTypeQuestion"></p>
-                    </div>
-                    <div class="imagen_content">
-                        <p>Imagen URL:</p>
-                        <input class="imagen" name="respuesta1" type="text"/>
-                    </div>
+                <div id='questions'>
+                    <div id="1" class='questionForm'>
+                        <div class='input-wrapper'>
+                            <label>Pregunta 1:</label>
+                            <input class='pregunta' name='respuesta1' type='text'/>
+                        </div>
+                        <div class='input-wrapper'>
+                            <label>Tipo de pregunta: </label>
+                            <select class="tipoPregunta" name="respuesta1" required>
+                                <option value="1">Abiertas</option>
+                                <option value="2">Selección Única</option>
+                                <option value="3">Ordenamiento</option>
+                                <option value="4">Selección Múltiple</option>
+                            </select>
+                            <p class="feedTypeQuestion"></p>
+                        </div>
+                        <div class='input-wrapper'>
+                            <label>Imagen URL:</label>
+                            <input class="imagen" name="respuesta1" type="text"/>
+                        </div>
 
-                    <div class="videoContent">
-                        <p>Video URL:</p>
-                        <input class="video" name="respuesta1" type="text"/>
-                    </div>
-                    <div id="Answers1">
+                        <div class='input-wrapper'>
+                            <label>Video URL:</label>
+                            <input class="video" name="respuesta1" type="text"/>
+                        </div>
+                        <div id="Answers1">
 
+                        </div>
+                        <span class='feedback'></span>
                     </div>
-                    <div class='feedback'></div>
                 </div>
-            </div>
-            <div class="enviarInfo">
-                <br/>
-                <button type='submit' id='addQuestion'>Agregar Pregunta</button>
+
+                <button type='submit' id='addQuestion' class='no-background'>Agregar Nueva Pregunta</button>
                 <button type='submit' id='submitQuestions'>Confimar Preguntas</button>
-                <br/>
             </div>
         </div>
     </section>
