455e9712a82289c5324dd252e7488b51549912d7
Alex <alexcris.m@gmail.com>
2016-09-06 20:12:11 -0500

    css for selected option

--- src/js/avances.js
+++ src/js/avances.js
@@ -1,6 +1,8 @@
 'use strict';
 
 $(document).on('ready', function () {
+    $('#avances-header-option').addClass('selected');
+
     setTimeout(function (event) {
         getEncuestas('avances');
     }, 500);
