dc971da7c0a35bbadb714ea8584d5280673465ac
Tenorio94 <geteca94@gmail.com>
2016-09-18 14:57:59 -0500

    Breakline

--- src/preguntas.php
+++ src/preguntas.php
@@ -59,6 +59,7 @@
                 <form action="upload.php" method="post" enctype="multipart/form-data">
                     Select image to upload:
                     <input type="file" name="fileToUpload" id="fileToUpload">
+                    <br/>
                     <input type="submit" value="Upload Image" name="submit">
                 </form>
             </div>
