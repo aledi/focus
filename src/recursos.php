079680f826fb71f85195c8311c95e49bb747f619
Tenorio94 <geteca94@gmail.com>
2016-09-18 19:00:18 -0500

    Resources tab

---
+++ src/recursos.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang='en'>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <script src="js/jquery-1.12.3.js"></script>
+    <link href='css/template.css' type='text/css' rel='stylesheet'/>
+    <!--<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js'></script>-->
+    <title> Focus - Encuestas</title>
+</head>
+
+<body>
+	<?php include_once('elements/header.php');?>
+	<section class='recursos-section'>
+		<div class='recursos-wrapper'>
+			<h2>Recursos</h2>
+		</div>
+	</section>
+</body>
+
+</html>
