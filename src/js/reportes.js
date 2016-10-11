c25e58705e9ba308631a44603557cd36232e3209
Tenorio94 <geteca94@gmail.com>
2016-10-11 18:25:59 -0500

    One column width

--- src/js/reportes.js
+++ src/js/reportes.js
@@ -137,6 +137,13 @@ function columnChart (opciones, votes, percent, chartNumber, title) {
             legend: {position: 'none'}
         };
 
+        if (opciones.length > 1){
+          options.bar.width = '80%';
+        }
+        else{
+          options.bar.width = '40%';
+        }
+
         options.title = title;
 
         var chart = document.getElementById('chart' + chartNumber);
