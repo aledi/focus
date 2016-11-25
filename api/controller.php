e2398a8874081a0fcc4c0912c2f2a2e7d87f58d2
Tenorio94 <geteca94@gmail.com>
2016-11-24 19:29:10 -0600

    Implements email

--- api/controller.php
+++ api/controller.php
@@ -462,28 +462,19 @@ function getMunicipiosFromFile() {
 
 function sendPassword ($email, $nombre, $password) {
 
-    $subject = 'Cambio de contraseña de panelista';
-    $message = 'Buenas tardes señor ' . $nombre . "\n\n" . 'Le mandamos un cordial saludo por parte del grupo Focus. Le informamos que su contraseña es ' . $password . ' si usted no solicitó un recordatorio de contraseña, le suplicamos que haga caso omiso a este mensaje.' . "\n\n" . 'Agradecemos sinceramente su preferencia y ¡Le deseamos un excelente día!';
-    $to = $email;
-    $type = 'plain'; // or HTML
-    $charset = 'utf-8';
+   $to = $email;
+    $subject = 'Focus - Recordatorio de Contraseña';
+    $from = 'atencion@focuscg.com.mx';
+     
+    $headers = 'From: ' . $from . "\r\n" .
+                'Reply-To:' . $from . "\r\n" .
+                'Return-Path: ' . $from . "\r\n" .
+                'MIME-Version: 1.0.' . "\r\n" .
+                'Content-Type: text/html; charset=UTF-8';
 
-    $mail     = 'no-reply@'.str_replace('www.', '', $_SERVER['SERVER_NAME']);
-    $uniqid   = md5(uniqid(time()));
-    $headers  = 'From: '.$mail."\n";
-    $headers .= 'Reply-to: '.$mail."\n";
-    $headers .= 'Return-Path: '.$mail."\n";
-    $headers .= 'Message-ID: <'.$uniqid.'@'.$_SERVER['SERVER_NAME'].">\n";
-    $headers .= 'MIME-Version: 1.0'."\n";
-    $headers .= 'Date: '.gmdate('D, d M Y H:i:s', time())."\n";
-    $headers .= 'X-Priority: 3'."\n";
-    $headers .= 'X-MSMail-Priority: Normal'."\n";
-    $headers .= 'Content-Type: multipart/mixed;boundary="----------'.$uniqid.'"'."\n\n";
-    $headers .= '------------'.$uniqid."\n";
-    $headers .= 'Content-type: text/'.$type.';charset='.$charset.''."\n";
-    $headers .= 'Content-transfer-encoding: 7bit';
+    $message = 'Buenas tardes señor ' . $nombre . "\n\n" . 'Le mandamos un cordial saludo por parte del grupo Focus. Le informamos que su contraseña es <strong>' . $password . '</strong> si usted no solicitó un recordatorio de contraseña, le suplicamos que haga caso omiso a este mensaje.' . "\n\n" . 'Agradecemos sinceramente su preferencia y ¡Le deseamos un excelente día!';
 
-     $retval = mail ($to, $subject, $message, $header);
+     $retval = mail ($to, $subject, $message, $headers);
      
      if( $retval == true ) {
         return "Message sent successfully...";
--- api/email.php
+++ api/email.php
@@ -4,20 +4,19 @@ echo json_encode(sendMail($_POST['email'], $_POST['username'], $_POST['password'
 function sendMail($email, $nombre, $password){
 	$to = $email;
 	$subject = 'Focus - Recordatorio de Contraseña';
-	$from = 'focus@no-reply.com';
+	$from = 'atencion@focuscg.com.mx';
 	 
 	// To send HTML mail, the Content-type header must be set
-	$headers = 'From: me@gmail.com' . '\r\n' .
-                'Reply-To: me@gmail.com' . '\r\n' .
-                'X-Mailer: PHP/' . phpversion() . '\r\n' .
-                'MIME-Version: 1.0\r\n' . '\r\n' .
-                'Content-Type: text/html; charset=ISO-8859-1\r\n';
+	$headers = 'From: ' . $from . "\r\n" .
+                'Reply-To:' . $from . "\r\n" .
+                'Return-Path: ' . $from . "\r\n" .
+                'MIME-Version: 1.0.' . "\r\n" .
+                'Content-Type: text/html; charset=UTF-8';
  
 	// Compose a simple HTML email message
 	$message = 'Buenas tardes señor ' . $nombre . "\n\n" . 'Le mandamos un cordial saludo por parte del grupo Focus. Le informamos que su contraseña es <strong>' . $password . '</strong> si usted no solicitó un recordatorio de contraseña, le suplicamos que haga caso omiso a este mensaje.' . "\n\n" . 'Agradecemos sinceramente su preferencia y ¡Le deseamos un excelente día!';
-	$message = wordwrap($message, 70, "\r\n");
 
-     $retval = mail ($to, $subject, $message, $header);
+     $retval = mail ($to, $subject, $message, $headers);
      
      if( $retval == true ) {
         return "Message sent successfully...";
