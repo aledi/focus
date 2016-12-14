fed7e0c6cfaf629b1977138de2add94ce641b3fc
Tenorio94 <geteca94@gmail.com>
2016-12-14 16:55:53 -0600

    Revert "Type mistake"
    This reverts commit 29e28942c46e2dbc6f154ba5aac9c600cb8db654.

--- src/js/liga-panel.js
+++ src/js/liga-panel.js
@@ -34,7 +34,7 @@ function checkAll (checkedBox) {
         return;
     }
 
-        currentChecked = checkedBox.checked ? tableSize : 0;
+    currentChecked += !checkedBox.checked ? tableSize : 0;
 
     for (var i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].parentElement.parentElement.style.display !== 'none' && checkboxes[i].className !== 'hidden') {
