b60eba81edf77d1ee7fc04bcd920cfc030806771
Eduardo Cristerna <eduardo@schoolhub.com>
2017-01-03 18:47:40 -0600

    Fixes #355 - Dates Issues

--- src/js/preguntas.js
+++ src/js/preguntas.js
@@ -228,7 +228,7 @@ $(document).on('ready', function () {
         lastQuestion = parseInt(lastQuestion) + 1;
 
         appendQuestions(lastQuestion);
-        
+
         $('#imagen' + lastQuestion).parent().find('label').hide();
         $('#video' + lastQuestion).parent().find('label').hide();
     });
